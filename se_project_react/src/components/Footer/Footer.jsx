6b7bf1c3033c18a846d092e37244db233aa7cc36
Obi-Pi-Kenobi <marcuscrawford91@gmail.com>
2024-06-18 10:18:34 -0500

    Add files via upload

---
+++ se_project_react/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h3 className="footer__text">Developed By Marcus Crawford</h3>
+      <h3 className="footer__text">2024</h3>
+    </footer>
+  );
+}
+
+export default Footer;
